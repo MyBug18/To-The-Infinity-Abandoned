ffda086ff5618bc19ecfc3c5d54bfe1145e02e01
Kevin Kim <mskimkim0318@gmail.com>
2019-11-25 22:48:41 +0900

    Added simple planet click detection

---
+++ Assets/Scripts/View/StarSystem/Planet/PlanetOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetOnClick : MonoBehaviour
+{
+    [SerializeField]
+    private PlanetWrapper _planetWrapper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(_planetWrapper.planet.name);
+    }
+}
