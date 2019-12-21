d67618c5934f3ca84c8f0f6d0778f01a66843470
Kevin Kim <mskimkim0318@gmail.com>
2019-12-21 19:48:13 +0900

    Code Refactoring - 2

--- Assets/Scripts/Model/Planet&POP/POPWorkingPlace/Districts/ElectricityDistrict.cs
+++ Assets/Scripts/Model/Planet&POP/POPWorkingPlace/Districts/ElectricityDistrict.cs
@@ -12,29 +12,13 @@ public class ElectricityDistrict : District
         districtType = DistrictType.Electricity;
         InitiallizePOPWorkingList(2);
 
+        workingPOPSlotList[0].upkeeps.Add(new JobUpkeep((GlobalResourceType.Money, 0.5f), null));
+        workingPOPSlotList[0].yields.Add(new JobYield((GlobalResourceType.Electricity, 1.5f), null));
         workingPOPSlotList[0].job = Job.Technician;
+
+        workingPOPSlotList[1].upkeeps.Add(new JobUpkeep((GlobalResourceType.Money, 0.5f), null));
+        workingPOPSlotList[1].yields.Add(new JobYield((GlobalResourceType.Electricity, 1.5f), null));
         workingPOPSlotList[1].job = Job.Technician;
-
-        List<JobUpkeep> upkeeps0 = new List<JobUpkeep>();
-        JobUpkeep _technicianUpkeep0 = new JobUpkeep((GlobalResourceType.Money, 0.5f), null);
-        upkeeps0.Add(_technicianUpkeep0);
-        workingPOPSlotList[0].upkeeps = upkeeps0;
-
-        List<JobUpkeep> upkeeps1 = new List<JobUpkeep>();
-        JobUpkeep _technicianUpkeep1 = new JobUpkeep((GlobalResourceType.Money, 0.5f), null);
-        upkeeps1.Add(_technicianUpkeep1);
-        workingPOPSlotList[1].upkeeps = upkeeps1;
-
-
-        List<JobYield> yield0 = new List<JobYield>();
-        JobYield _technicianYield0 = new JobYield((GlobalResourceType.Electricity, 1.5f), null);
-        yield0.Add(_technicianYield0);
-        workingPOPSlotList[0].yields = yield0;
-
-        List<JobYield> yield1 = new List<JobYield>();
-        JobYield _technicianYield1 = new JobYield((GlobalResourceType.Electricity, 1.5f), null);
-        yield1.Add(_technicianYield1);
-        workingPOPSlotList[1].yields = yield1;
 
     }    
 
--- Assets/Scripts/Model/Planet&POP/POPWorkingPlace/Districts/MineralDistrict.cs
+++ Assets/Scripts/Model/Planet&POP/POPWorkingPlace/Districts/MineralDistrict.cs
@@ -12,28 +12,13 @@ public class MineralDistrict : District
         districtType = DistrictType.Mineral;
         InitiallizePOPWorkingList(2);
 
+        workingPOPSlotList[0].upkeeps.Add(new JobUpkeep((GlobalResourceType.Money, 0.5f), null));
+        workingPOPSlotList[0].yields.Add(new JobYield((GlobalResourceType.Mineral, 6f), null));
         workingPOPSlotList[0].job = Job.Miner;
+
+        workingPOPSlotList[1].upkeeps.Add(new JobUpkeep((GlobalResourceType.Money, 0.5f), null));
+        workingPOPSlotList[1].yields.Add(new JobYield((GlobalResourceType.Mineral, 6f), null));
         workingPOPSlotList[1].job = Job.Miner;
-
-        List<JobUpkeep> upkeeps0 = new List<JobUpkeep>();
-        JobUpkeep _minerUpkeep0 = new JobUpkeep((GlobalResourceType.Money, 0.5f), null);
-        upkeeps0.Add(_minerUpkeep0);
-        workingPOPSlotList[0].upkeeps = upkeeps0;
-
-        List<JobUpkeep> upkeeps1 = new List<JobUpkeep>();
-        JobUpkeep _minerUpkeep1 = new JobUpkeep((GlobalResourceType.Money, 0.5f), null);
-        upkeeps1.Add(_minerUpkeep1);
-        workingPOPSlotList[1].upkeeps = upkeeps1;
-
-        List<JobYield> yield0 = new List<JobYield>();
-        JobYield _minerYield0 = new JobYield((GlobalResourceType.Mineral, 6f), null);
-        yield0.Add(_minerYield0);
-        workingPOPSlotList[0].yields = yield0;
-
-        List<JobYield> yield1 = new List<JobYield>();
-        JobYield _minerYield1 = new JobYield((GlobalResourceType.Mineral, 6f), null);
-        yield1.Add(_minerYield1);
-        workingPOPSlotList[1].yields = yield1;
     }
 
     public override void OnConstructing()
